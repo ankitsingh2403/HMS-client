526978f7aaf27b720528bf54661117d16b667f75
Ankit Kumar Singh <110078839+ankitsingh2403@users.noreply.github.com>
2025-05-08 21:07:28 +0530

    Update MessageForm.jsx

--- Frontend/src/components/MessageForm.jsx
+++ Frontend/src/components/MessageForm.jsx
@@ -2,8 +2,9 @@ import axios from "axios";
 import React, { useState } from "react";
 import { toast } from "react-toastify";
 
-// ✅ Dynamic base URL setup
-const API_BASE_URL = import.meta.env.VITE_API_URL || ""; // Uses VITE_API_URL from environment variable
+const API_BASE_URL =
+  import.meta.env.MODE === "production" ? "" : "http://localhost:4000";
+
 
 
 const MessageForm = () => {
